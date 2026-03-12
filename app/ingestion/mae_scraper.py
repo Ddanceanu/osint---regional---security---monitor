67113094c73742cf3024d067d81b6055c95349f5
Ddanceanu <80147530+Ddanceanu@users.noreply.github.com>
2026-03-12 11:46:57 +0200

    Add basic MAE source access test with request headers and error handling.

--- app/ingestion/mae_scraper.py
+++ app/ingestion/mae_scraper.py
@@ -1,4 +1,5 @@
 from app.ingestion.base_scraper import BaseScraper
+import requests
 
 class MaeScraper(BaseScraper):
     """
@@ -13,12 +14,29 @@ class MaeScraper(BaseScraper):
             source_name="MAE Romania",
             source_type="official"
         )
+        self.base_url = "https://www.mae.ro/en"
 
     def fetch_documents(self) -> list[dict]:
         """
-        Return document candidates collected from the MAE source.
-
-        For now, this method returns an empty list because
-        the scraping logic is not implemented yet.
+        Test access to the MAE source and return am empty list for now.
         """
+
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/122.0.0.0 Safari/537.36"
+            )
+        }
+
+        try:
+            response = requests.get(self.base_url, headers=headers, timeout = 10)
+
+            print(f"Fetching from: {self.base_url}")
+            print(f"Status code: {response.status_code}")
+
+        except requests.RequestException as e:
+            print(f"Failed to fetch source: {self.base_url}")
+            print(f"Error: {e}")
+
         return []
